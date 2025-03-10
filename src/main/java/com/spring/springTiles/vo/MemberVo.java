334bd05b00f196203d863b4de6613f96f79ff38b
kar1208 <beorc8686@naver.com>
2025-03-10 19:39:42 +0900

    20250310_1939 springTiles 수정1번

---
+++ src/main/java/com/spring/springTiles/vo/MemberVo.java
@@ -0,0 +1,14 @@
+package com.spring.springTiles.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class MemberVo {
+	private String mid;
+	private String pwd;
+	private String idSave;
+}
